08fc3688d7d563447b8b1bffbb908bdc0b8b38bc
Amboolents <149694170+AnToV7777@users.noreply.github.com>
2024-12-27 23:03:16 +0100

    init commit branch

--- DbContext/Migrations/SqlServerDbContext/20241227215325_miInitial.Designer.cs
+++ DbContext/Migrations/SqlServerDbContext/20241227215325_miInitial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DbContext.Migrations.SqlServerDbContext
 {
     [DbContext(typeof(MainDbContext.SqlServerDbContext))]
-    [Migration("20241204151926_miInitial")]
+    [Migration("20241227215325_miInitial")]
     partial class miInitial
     {
         /// <inheritdoc />
